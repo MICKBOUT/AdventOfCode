a0196253601ab10132daa7f483e275754f12f9f4
MICKBOUT <matisse.boutte@gmail.com>
2025-12-03 16:13:29 +0100

    day 1 + 2

---
+++ day02/day02.py
@@ -0,0 +1,42 @@
+import time
+
+input_test = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
+data = "5959566378-5959623425,946263-1041590,7777713106-7777870316,35289387-35394603,400-605,9398763-9592164,74280544-74442206,85684682-85865536,90493-179243,202820-342465,872920-935940,76905692-76973065,822774704-822842541,642605-677786,3759067960-3759239836,1284-3164,755464-833196,52-128,3-14,30481-55388,844722790-844967944,83826709-83860070,9595933151-9595993435,4216-9667,529939-579900,1077949-1151438,394508-486310,794-1154,10159-17642,5471119-5683923,16-36,17797-29079,187-382"
+	
+def add_mirror(start, end):
+	res = 0
+	for nb in range(start, end + 1):
+		s = str(nb)
+		m = len(s) >> 1
+		if s[:m] == s[m:]:
+			res += nb
+	return res
+
+def add_mirror_v2(start, end):
+	res = 0
+	for nb in range(start, end + 1):
+		s = str(nb)
+		l = len(s)
+		m = l >> 1
+		while m > 0:
+			if (l % m == 0):
+				j = 0
+				while (s[j*m:(j+1)*m] == s[(j+1)*m : (j+2)*m]):
+					j += 1
+				if j + 1 == (l // m):
+					res += nb
+					break
+			m -= 1
+	return res
+
+def day02(s, part):
+	res = 0
+	range_nb = s.split(',')
+	for r in range_nb:
+		start, end = r.split('-')
+		res += add_mirror(int(start), int(end)) if part == 0 else add_mirror_v2(int(start), int(end))
+	return res
+
+for i in range(2):
+	s = time.time()
+	print(f"day02 pb{i} : res = {day02(data, i)} in {round(time.time() - s, 3)}s")

507cb6d330a9248ca42bfe115e6d6bf004aca182
MICKBOUT <matisse.boutte@gmail.com>
2025-12-08 11:28:19 +0100

    refactor

--- day08/day08.py
+++ day08/day08.py
@@ -1,25 +1,28 @@
-input_file = "input.txt"
 import time
+from math import sqrt, dist
+
+input_file = "input.txt"
+
 with open(input_file, "r") as file:
 	arrey = [tuple(map(int, line.strip().split(','))) for line in file]
 
-from math import sqrt, dist
-import time
-
-def day08_pt1(arrey):
+def ft_creat_lst(arrey, len_arrey):
 	lst = []
-	len_arrey = len(arrey)
-	groups_index = [x for x in range(len_arrey)]
-	groups = {x:[x] for x in range(len_arrey)}
 	for i in range(len_arrey - 1):
 		x1, y1, z1 = arrey[i]
 		for j in range(i + 1, len_arrey):
 			x2, y2, z2 = arrey[j]
 			ell = (dist((x1, y1, z1), (x2, y2, z2)), i, j)
 			lst.append(ell)
-
 	lst.sort(key= lambda x:x[0])
-	
+	return (lst)
+
+def day08_pt1(arrey):
+	len_arrey = len(arrey)
+	groups_index = [x for x in range(len_arrey)]
+	groups = {x:[x] for x in range(len_arrey)}
+	lst = ft_creat_lst(arrey, len_arrey)
+
 	for _, index_p1, index_p2 in lst[:1000]:
 		gp_p1, gp_p2 = groups_index[index_p1], groups_index[index_p2]
 		if gp_p1 == gp_p2:
@@ -34,13 +37,9 @@ def day08_pt1(arrey):
 	m1, m2, m3 = 0, 0, 0
 	for key, value in groups.items():
 		l = len(value)
-		if l > m1:
-			m1, m2, m3 = l, m1, m2
-		elif l > m2:
-			m2, m3 = l, m2
-		elif l > m3:
-			m3 = l
-	
+		if l > m1: m1, m2, m3 = l, m1, m2
+		elif l > m2: m2, m3 = l, m2
+		elif l > m3: m3 = l
 	return(m1 * m2 * m3)
 	
 def day08_pt2(arrey):
@@ -48,15 +47,7 @@ def day08_pt2(arrey):
 	len_arrey = len(arrey)
 	groups_index = [x for x in range(len_arrey)]
 	groups = {x:[x] for x in range(len_arrey)}
-	
-	for i in range(len_arrey - 1):
-		x1, y1, z1 = arrey[i]
-		for j in range(i + 1, len_arrey):
-			x2, y2, z2 = arrey[j]
-			ell = (dist((x1, y1, z1), (x2, y2, z2)), i, j)
-			lst.append(ell)
-
-	lst.sort(key= lambda x:x[0])
+	lst = ft_creat_lst(arrey, len_arrey)
 	
 	for _, index_p1, index_p2 in lst:
 		gp_p1, gp_p2 = groups_index[index_p1], groups_index[index_p2]
